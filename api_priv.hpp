796fd64691bd5f1af068e63ea236b83397ec6132
haukurhafsteins <haukurhafsteins@gmail.com>
2024-05-30 10:04:51 +0000

    Bufsized changed to 1024

--- api_priv.hpp
+++ api_priv.hpp
@@ -6,7 +6,7 @@
 #include "esp_http_server.h"
 
 #define BOUNDARY_MAX_LEN 100
-#define API_BUFFSIZE 512
+#define API_BUFFSIZE 1024
 #define FILE_PATH_MAX (ESP_VFS_PATH_MAX + 64)
 
 bool get_value_from_query(httpd_req_t *req, const char* value_name, char *destination, size_t dest_len);
